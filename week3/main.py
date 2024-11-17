160836a55970816e3f14dad091dd1ae64f1f4e65
Katyuska Lahdemaki Zuasnabar <Katyuska.Lahdemaki.Zuasnabar@student.lab.fi>
2024-11-18 00:05:47 +0200

    Week3: Added password generation -function.

--- week3/main.py
+++ week3/main.py
@@ -1,3 +1,4 @@
+import random
 
 # available user rights
 USER_RIGHTS = ["User", "Admin", "Super-User"]
@@ -22,8 +23,49 @@ def check_duplicate_user(username):
             return True
     return False
 
-def generate_password(name, lastname, age):
-    return "placeholder"
+def generate_password():
+    # ask the user for the numbers of different category of characters
+    # capital letters in ASCII are from number 65 to 90
+    # lower case letters in ASCII are from number 97 to 122
+    # numbers are from 48 to 57
+    # special characters are from 32 to 47, 59 - 64, 91 - 96 and 123 - 126
+    # we generate a random number and then we convert it to ASCII character with the knowledge above
+    # for example chr(72) converts to 'H'
+    user_happy = False
+    while not user_happy:
+        print("Generating password...")
+        nof_capital_letters = int(input("How many capital letters you want? "))
+        nof_lower_letters= int(input("How many lower letters you want? "))
+        nof_numbers= int(input("How many numbers you want? "))
+        nof_special_characters= int(input("How many special characters you want? "))
+        capital_letters = []
+        for i in range(0, nof_capital_letters):
+            capital_letters.append(chr(random.randrange(65, 90)))
+        low_letters = []
+        for i in range(0, nof_lower_letters):
+            low_letters.append(chr(random.randrange(97, 122)))
+        numbers = []
+        for i in range(0, nof_numbers): 
+            numbers.append(chr(random.randrange(48,57)))
+        special_characters = []
+        SPECIAL_RANGES = [
+            (32, 47),
+            (59, 64),
+            (91, 96),
+            (123, 126)
+        ]
+        for i in range(0, nof_special_characters):
+            range_index = random.randrange(0, len(SPECIAL_RANGES) - 1)
+            special_characters.append(
+                chr(random.randrange(
+                    SPECIAL_RANGES[range_index][0], 
+                    SPECIAL_RANGES[range_index][1])
+                    ))
+        password = "".join(capital_letters) + "".join(numbers) + "".join(low_letters) + "".join(special_characters)
+        print(f"Generated password: {password}")
+        user_happy_query = input("Are you happy with the password? [y, n] ")
+        user_happy = user_happy_query == 'y'
+    return password 
 
 def ask_user_details():
     global users_list
@@ -36,7 +78,7 @@ def ask_user_details():
         username = input("Duplicate user detected. Please enter new username")
 
     rights = query_user_rights()
-    password = generate_password(name, lastname, age)
+    password = generate_password()
     # Dictionary of a user
     user =  {
         "name": " ".join([name, lastname]), 
@@ -45,12 +87,13 @@ def ask_user_details():
         "rights": rights, 
         "password": password
         }
+    print (password)
     return user
 
 def main():
     global users_list # Allows us to access to global variable 'users_list'
     while True:
-        print(f"{len(users_list)} Users in the system: ")
+        print(f"{len(users_list)} users in the system: ")
         for u in users_list:
             print(f"{u["username"]}: {u["rights"]}")
         user = ask_user_details()
