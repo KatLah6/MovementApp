29122f301bfc78dd4816a62fb8696e7f5dfe82a8
Katyuska Lahdemaki Zuasnabar <Katyuska.Lahdemaki.Zuasnabar@student.lab.fi>
2024-11-17 16:34:48 +0200

    Added week2 implementation

---
+++ week2/main.py
@@ -0,0 +1,28 @@
+
+# Week 2 - Movement App
+
+
+#Temperature_Format=input("Do you want the temperature in °C or °F? ")
+Teacher= "Mira"
+Myself = "Katyuska"
+Age= "<>18"
+#Get user name
+Name=input("What is your name? ")
+
+
+#Check user
+if Name == Teacher:
+    print("you have super-user rights.")
+elif Name == Myself:
+    print("you have admin rights.")
+else:
+    age=int(input("What is your age? "))
+
+#Check age
+if Name != {Myself} or {Teacher} and age ==18:
+    print("You have viewer rights")
+if Name != {Myself} or {Teacher} and age >18:
+    print("You have viewer rights")
+if Name != {Myself} or {Teacher} and age <18:
+    print(f"Grettings {Name} you are too young to operate this program. ")
+    print("Thanks for visiting.")
